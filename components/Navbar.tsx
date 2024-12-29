52195d640e50a2bc418b1d6449e0bf11f7cd5288
Talha Jamal <chrisLandson057@gmail.com>
2024-12-29 22:10:56 +0500

    Added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,8 @@ function Navbar() {
   return (
     <div className=' max-w-6xl m-auto w-[90%]'>
       <nav className=' bg-white shadow-lg p-3 md:p-4 mt-4 flex justify-between items-center rounded-lg'>
-        <Link href='/' className=' bg-gradient-to-r from-gray-600 via-gray-700 to-gray-800 bg-clip-text text-transparent font-semibold text-2xl' >
-          Store
+        <Link href='/' className=' text-black font-semibold text-2xl' >
+          ShopEasy
         </Link>
         <div className=' flex items-center space-x-4'>
           <CartPanel />
